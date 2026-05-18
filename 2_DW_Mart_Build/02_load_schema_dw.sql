5476801fc37e2db3ab46ad0005e05b3606cd2839
Nickson Cheruiyot <nickcheruiyot16@gmail.com>
2026-05-18 11:07:44 +0300

    build and validate flat mart

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -23,7 +23,7 @@ INSERT INTO job_postings_fact (
     job_country, salary_rate, salary_year_avg, salary_hour_avg
 )
 SELECT
-    job_id, company_id, job_title, job_title, job_location,
+    job_id, company_id, job_title_short, job_title, job_location,
     job_via, job_schedule_type, job_work_from_home, search_location,
     job_posted_date, job_no_degree_mention, job_health_insurance,
     job_country, salary_rate, salary_year_avg, salary_hour_avg
